76a27af8bbe5521e9fa37e665ae1286960dc7c7c
danmyway <Sirius747487!>
2021-03-11 23:29:50 +0100

    variables now assigned default values which are working in case of empty input #hackerman

--- RequestGen.py
+++ RequestGen.py
@@ -1,14 +1,27 @@
+import sys
+
 def ReqForm():
+
     blank_space = "_"*20
-    office = blank_space
-    full_name = blank_space
-    birth_date = blank_space
-    addr_of_res = blank_space
+    office = input("Adresa obecniho uradu v miste Vaseho volebniho okrsku: ") or blank_space
+    full_name = input("Vase cele jmeno: ") or blank_space
+    birth_date = input("Vase datum narozeni: ") or blank_space
+    addr_of_res = input("Vase trvale bydliste: ") or blank_space
     election_type = blank_space
     election_date = blank_space
 
 
-    req_text = "Obecní úřad {office}".format(office=input("Adresa obecniho uradu "))
+    req_text = "Obecní úřad " \
+               "\n{}" \
+               "\n" \
+               "\n" \
+               "\nZADOST O VYDANI VOLICSKEHO PRUKAZU" \
+               "\npro hlasovani ve volbach" \
+               "\n" \
+               "\n" \
+               "\nJmeno a prijmeni zadatele: {}" \
+               "\nDatum narozeni: {}" \
+               "\nTrvaly pobyt: {}".format(office, full_name, birth_date, addr_of_res)
 
 
 
@@ -17,5 +30,7 @@ def ReqForm():
             req.write(req_text)
     except OSError as e:
         print(e)
+        raise
+    sys.exit(99)
 
 ReqForm()
